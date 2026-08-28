91d3611e3d7fa971bca288d7b16acd62b20e6d61
Line <laln@itu.dk>
2026-08-28 14:21:18 +0200

    BISON.CLI.csproj updated to be able to read file. Now printing in wrong format, but printing.
    Co-authored-by: laeveroest <laeveroest@users.noreply.github.com>
    Co-authored-by: AnneVestbo <Anne174-boop@users.noreply.github.com>
    Co-authored-by: Nicklas Stefansen <NicklasReeves@users.noreply.github.com>

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿foreach (var arg in args)
-Console.WriteLine(arg);
+﻿using System;
+using System.IO;
+
+class Program 
+{
+    
+    static void Main(string[] args)
+    {
+        string filePath = "bison_observe_cli_db.csv";
+    
+        foreach (string Line in File.ReadLines(filePath))
+        {
+        Console.WriteLine(Line);
+        }
+    }
+    
+}
--- obj/Debug/net8.0/Bison.CLI.AssemblyInfo.cs
+++ obj/Debug/net8.0/Bison.CLI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bison.CLI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19011f18aa1eb8685e3b9b369b12cbcff0b43c33")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+704156b581bc540e2af5e37fb2659ce7806fb3a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bison.CLI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bison.CLI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
