d85837f25a14f0b5402a39e8e65d8ce2bb858314
Anne <akjn@itu.dk>
2026-08-30 13:54:48 +0200

    Formatted text for the timestamp <3

--- Program.cs
+++ Program.cs
@@ -1,16 +1,28 @@
 ﻿using System;
 using System.IO;
 
-class Program 
-{
+class Program {
     
-    static void Main(string[] args)
-    {
+    static void Main(string[] args) {
         string filePath = "bison_observe_cli_db.csv";
-    
-        foreach (string Line in File.ReadLines(filePath))
-        {
-        Console.WriteLine(Line);
+        int lineNumber = 0;    
+
+        foreach (string Line in File.ReadLines(filePath)) {
+            lineNumber++;
+            if(lineNumber==1){
+                continue;
+            }
+            string[] info = Line.Replace('"',' ').Split(',');
+
+            string author =info[0];
+            string observation = info[1];
+            long timestamp = long.Parse(info[2]);
+
+            DateTime dateTime = DateTimeOffset.FromUnixTimeSeconds(timestamp).LocalDateTime;
+
+            string formattedDateTime = dateTime.ToString("yyyy-MM-dd HH:mm:ss");
+
+            Console.WriteLine(author + " @ " + formattedDateTime + " : " + observation);
         }
     }
     
