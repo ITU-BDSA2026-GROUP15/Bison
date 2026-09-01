ac167a20299d1e1b34f001cc4298df048cebf34a
Line Alma Breitenstein Nissen <laln@itu.dk>
2026-09-01 17:46:58 +0200

    Refactored code, readFile is now a method

--- Program.cs
+++ Program.cs
@@ -3,26 +3,10 @@ using System.IO;
 
 class Program {
     public static void Main() {
-        
+    
+        //StreamReader needs a try/catch block
         try {
-            using (StreamReader sr = new StreamReader("bison_observe_cli_db.csv")) {
-                //read headline and not print it in console
-                string headLine = sr.ReadLine();
-                
-                string line;
-
-                while ((line =sr.ReadLine()) != null) {
-                    string[] values = line.Split(',');
-                    
-                    string author = values[0];
-                    string observation = values[1];
-                    string timestamp = values[2]; 
-                    
-                    Console.WriteLine("Author: " + author);
-                    Console.WriteLine("Observation: " + observation);
-                    Console.WriteLine("Timestamp: " + timestamp);
-                }
-            }
+            readFile();
         }
 
         catch (Exception e) {
@@ -30,4 +14,38 @@ class Program {
             Console.WriteLine(e.Message);
         }
     }
+
+    public static void readFile() {
+        using (StreamReader sr = new StreamReader("bison_observe_cli_db.csv")) {
+                //read headline and not print it in console
+                string headLine = sr.ReadLine();
+                
+                //get the next line
+                string line;
+
+                while ((line =sr.ReadLine()) != null) {
+                    string[] values = line.Split(',');
+                    
+                    //format wants author in upper
+                    string author = values[0].ToUpper();
+                    string observation = values[1];
+                    string timestamp = values[2]; 
+                    
+                    Console.WriteLine(author + " @ " + timestamp + " " + observation);
+
+                }
+            }
+    }
+
+/*
+    public static append (string line) {
+        //creating a path to the file where we want to append
+        using (StreamWriter sw = File.CreateText(sr)) {
+            sw.WriteLine(author);
+            sw.Write(" @ ") ;
+            sw.Write(timestamp);
+            sw.Write(observation);
+        }
+    }
+*/
 }
