a8a45f8e677bc274252fc61ac3e3b8757c4ce558
Line Alma Breitenstein Nissen <laln@itu.dk>
2026-09-01 17:22:56 +0200

    Succesfully splitting input in array

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿var arg in args)
- Console.WriteLine(arg);
+﻿using System;
+using System.IO;
+
+class Program {
+    public static void Main() {
+        
+        try {
+            using (StreamReader sr = new StreamReader("bison_observe_cli_db.csv")) {
+                string line;
+
+                while ((line =sr.ReadLine()) != null) {
+                    string[] values = line.Split(',');
+                    
+                    string author = values[0];
+                    string observation = values[1];
+                    string timestamp = values[2]; 
+                    
+                    Console.WriteLine("Author: " + author);
+                    Console.WriteLine("Observation: " + observation);
+                    Console.WriteLine("Timestamp: " + timestamp);
+                }
+            }
+        }
+
+        catch (Exception e) {
+            Console.WriteLine("File could not be read: ");
+            Console.WriteLine(e.Message);
+        }
+    }
+}
