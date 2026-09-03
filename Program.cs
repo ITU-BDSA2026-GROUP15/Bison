7ccc9a92993646eff313368d315eb2a29be5cf2f
Anne <akjn@itu.dk>
2026-09-03 15:02:58 +0200

    text and a default case if there is no arugments in the list args

--- Program.cs
+++ Program.cs
@@ -2,14 +2,18 @@
 using System.IO;
 
 class Program {
-    public static void Main(string[] args) {
+    public static void Main(string[] args) {//args is what you write in the terminal after the program name, for example: dotnet run observe
         
+        if(args.Length == 0) {//default case if no arguments are provided
+            Console.WriteLine("Please provide an argument: read or observe");
+            return;
+        }
         if (args[0].Equals("read")) {
             read();
         }
 
         if (args[0].Equals("observe")) {
-            string observation = args[1];
+            string observation = args[1];// this is a range operator, it takes all the elements from index 1 to the end of the array.
             observe(observation);
         }
 
@@ -37,6 +41,8 @@ class Program {
                     string observation = values[1].Trim('"');
 
                     string timestamp = values[2]; 
+
+
                     DateTimeOffset time = convertTime(timestamp);
                     
                     Console.WriteLine(author + " @ " + time.ToString("MM/dd/yy HH:mm:ss") + ": " + observation);
@@ -58,19 +64,27 @@ class Program {
         long unixSeconds = long.Parse(timestamp);
         
         //now using the DTO library to convert unix seconds into actual time
+        //is for converting the timetamp into local time, so that it is easier to read for the user
         DateTimeOffset time = DateTimeOffset.FromUnixTimeSeconds(unixSeconds).ToLocalTime();
         
         //returning the time back to the formatting
         return time;
     }
 
-    public static void observe(string line) {
-            string observation = line;
+    public static void observe(string observation) {
+    
+            //Environment.UserName gets the username of the person running the program, and uses it as the author of the observation
+            //from https://learn.microsoft.com/en-us/dotnet/api/system.environment.username?view=net-10.0
             string author = Environment.UserName;
             
+            // gets the current time in seconds since 1970-01-01 00:00:00 UTC, and uses it as the timestamp of the observation, link from week 1
             DateTimeOffset localtime = DateTimeOffset.Now;
+
+
             long time = localtime.ToUnixTimeSeconds();
            
+            //https://stackoverflow.com/questions/18757097/writing-data-into-csv-file-in-c-sharp
+           //AppendText is used to append the new observation to the end of the file, and not overwrite the existing observations
             using (StreamWriter sw = File.AppendText("bison_observe_cli_db.csv")) {
                 sw.WriteLine($"\"{author},\"\"{observation}\"\",{time}\"");
             }
