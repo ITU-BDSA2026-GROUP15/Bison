46e0ab287a6e6628889411355d67052fa5af2526
Anne <akjn@itu.dk>
2026-09-03 22:44:33 +0200

    i found a mistake when i use the command: dotnet run -- read extra. It is not suppose to execute the data from the csv file

--- ReadOptions.cs
+++ ReadOptions.cs
@@ -4,4 +4,6 @@ using CommandLine;
 
 public class ReadOptions{
     //Empty because read does not require any additional input from the user.
-    }
+}
+//there is a bug: when i write dotnet run -- read extra it is suppose to give me an error
+// it does not.
