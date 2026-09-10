6faeb5424481fb1fffd1589c9bdac45beb15e995
Anne <akjn@itu.dk>
2026-09-10 11:57:10 +0200

    now I have started building

--- LocationOptions.cs
+++ LocationOptions.cs
@@ -5,4 +5,5 @@ using CommandLine;
 public class LocationOptions
 {
     [Value(0,MetaName ="location",Required =true, HelpText ="The location to search for")]
+    public string Location{get;set;} string.Empty;
 }
