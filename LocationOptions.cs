c4b87d24f983991fd02f24a3a8f577ea7142613b
Anne <akjn@itu.dk>
2026-09-10 12:00:18 +0200

    I think I have made the changes that needs to be in the LocationOptions.cs

--- LocationOptions.cs
+++ LocationOptions.cs
@@ -1,9 +1,9 @@
 using CommandLine;
 
-[Verb("location", HelpText ="Show observation from the location")]
+[Verb("location",HelpText ="Show observation from the location")]
 
 public class LocationOptions
 {
     [Value(0,MetaName ="location",Required =true, HelpText ="The location to search for")]
-    public string Location{get;set;} string.Empty;
+    public string Location{get;set;} = string.Empty;
 }
