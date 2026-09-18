c228fdf363fec6bbcb1bf5b90f96365d0b29a3ae
Lærke Vestergaard Roest <lroe@itu.dk>
2026-09-18 12:56:47 +0200

    First attemtt at refactoring for 1a) for the db in Bison.Razor
    Co-authored-by: AnneVestbo <Anne174-boop@users.noreply.github.com>
    Co-authored-by: Jon Theodor Tärnberg <jt.taernberg@hotmail.com>
    Co-authored-by: librni <librni@users.noreply.github.com>
    Co-authored-by: Nicklas Stefansen <NicklasReeves@users.noreply.github.com>

--- src/Bison.Razor/Program.cs
+++ src/Bison.Razor/Program.cs
@@ -4,6 +4,18 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
-app.MapGet("/observations", () => new Observation("Peter", "I saw the heron again!", 1684229348));
+var observationDB= new CSVDatabase<Cheep>("bison_observe_cli_db.csv");
+var commentDb = new CSVDatabase<Cheep>("bison_comment_cli_db.csv");
+
+
+// skal sende et kald til loggede observationer i stedet?? -> connecte dette til simpledb?
+app.MapGet("/observations", () => observationDb.Read("bison_observe_cli_db.csv")); 
+app.MapPost("/observation", (Cheep observation) => observationDb.Store(observation));
+
+app.MapGet("/comments", (int id)=> Comment("Author",id, "Observation"));
+app.MapPost("/comment", ()=> new Comment("Author",id, "Observation"));
+//er der noget som siger return all comment?
+//hvordan ved vi at den kommer ind i appsettings.json?
+
 app.Run();
 public record Observation(string Author, string Message, long Timestamp);
