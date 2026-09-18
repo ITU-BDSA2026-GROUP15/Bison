9f84b6b31470ffd51e202351cb9ff3c10ccb5c9c
Anne <akjn@itu.dk>
2026-09-18 18:34:27 +0200

    added Taxons from learnIT to the project.  changes in the program.cs from razor to fit the project more

--- src/Bison.Razor/Program.cs
+++ src/Bison.Razor/Program.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.Design;
 using Microsoft.VisualBasic;
 using SimpleDB;
 
@@ -8,17 +9,36 @@ var app = builder.Build(); //building the webapplication itself (metadata)
 
 string obsFile = "../../bison_observe_cli_db.csv";
 string comFile = "../../bison_comment_cli_db.csv";
-
+string propFile = "../../bison_propose_cli_db.csv";
 
 var observationDb = CSVDatabase<Cheep>.Instance;
 var commentDb = CSVDatabase<Cheep>.Instance;
+var proposalDb = CSVDatabase<Cheep>.Instance;
 
 // skal sende et kald til loggede observationer i stedet?? -> connecte dette til simpledb?
 app.MapGet("/observations", () => observationDb.Read(obsFile)); 
 
 app.MapPost("/observation", (Cheep observation) => {
     observationDb.Store(obsFile,observation); 
+});
 
+// Proposals
+app.MapGet("/proposals", (taxonID) => proposalDb.Read(propFile));
+
+app.MapPost("/proposal", (Cheep observation) => {
+    var allProposals = proposalDb.Read(propFile);
+    var matchingProposal = new List<Cheep>();
+    foreach(var proposal in allProposals) {
+        if (proposal.ID == id) {
+            matchingProposal.Add(proposal);
+        }
+        else {
+            Console.WriteLine("invalid observation ID");
+        }
+        return matchingPorposal;
+    }
+
+    proposalDb.Store(propFile,proposal); 
 });
 
 //MAPGET -> gets the observations/ the comments
@@ -30,10 +50,9 @@ app.MapPost("/observation", (Cheep observation) => {
 app.MapGet("/comments", (int id) => {
     var allComments = commentDb.Read(comFile);
     var matchingComment = new List<Cheep>();
-    foreach(var comment in allComments)
-    {
-        if (comment.ID == id)
-        {
+    foreach(var comment in allComments){
+        
+        if (comment.ID == id){
             matchingComment.Add(comment);
         }
     }
