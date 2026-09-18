2176f786b7eba9573f7957c19900080628b378ff
Lærke Vestergaard Roest <lroe@itu.dk>
2026-09-18 13:56:53 +0200

    Implemented HTTPS POST/GET to the endpoints in our CSV Database Web service at its current stage
    Co-authored-by: AnneVestbo <Anne174-boop@users.noreply.github.com>
    Co-authored-by: Jon Theodor Tärnberg <jt.taernberg@hotmail.com>
    Co-authored-by: librni <librni@users.noreply.github.com>
    Co-authored-by: Nicklas Stefansen <NicklasReeves@users.noreply.github.com>

--- src/Bison.Razor/Program.cs
+++ src/Bison.Razor/Program.cs
@@ -1,10 +1,8 @@
-using Microsoft.AspNetCore.Builder;
-using SimpleDB.CSVDatabase;
-
+using SimpleDB;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var app = builder.Build();
+var app = builder.Build(); //building the webapplication itself (metadata)
 
 string obsFile = "bison_observe_cli_db.csv";
 string comFile = "bison_comment_cli_db.csv";
@@ -14,12 +12,16 @@ var commentDb = new CSVDatabase<Cheep>.Instance(CSVDatabase.cs);
 
 // skal sende et kald til loggede observationer i stedet?? -> connecte dette til simpledb?
 app.MapGet("/observations", () => observationDb.Read(obsFile)); 
-app.MapPost("/observation", (Cheep observation) => observationDb.Store(observation));
+app.MapPost("/observation", (Cheep observation) => {
+    return observationDb.Store(observation); });
 
 app.MapGet("/comments", (int id)=> commentDb.Read(comFile));
-app.MapPost("/comment", (Cheep comment)=>  commentDb.Store(comment));
+//app.MapGet("/comments", () => commentDb.Read(comFile)); 
+
+app.MapPost("/comment", (Cheep comment)=>  { 
+    return commentDb.Store(comment);});
+
 //er der noget som siger return all comment?
 //hvordan ved vi at den kommer ind i appsettings.json?
 
 app.Run();
-public record Observation(string Author, string Message, long Timestamp);
