88456810624cdf84ea3f622de4b0b0bca4e5bf7b
JonTheo1999 <jt.taernberg@hotmail.com>
2026-09-20 11:41:39 +0200

    Deleted duplicate Cheep.cs. Cheep.cs now only exists in the database. All files that use cheeps references simpleDB

--- src/Bison.CLI/Cheep.cs
+++
@@ -1,4 +0,0 @@
-
-public record Cheep(string Author, int ID, string Observation, long Timestamp, string Location);
-// OBS: Cheep findes nu 2 steder og er ren kodeduplikation. Skal nok slettes i Bison.CLI,
-// da både Bison.CLI og Bison.razor kan bruge cheep fra simpleDB.
--- src/Bison.CLI/UserInterface.cs
+++ src/Bison.CLI/UserInterface.cs
@@ -1,4 +1,5 @@
 using System;
+using SimpleDB;
 
 static class UserInterface
 {
