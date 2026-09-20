85cb9824055335c2c96401de5998327a23fd7d3f
JonTheo1999 <jt.taernberg@hotmail.com>
2026-09-20 11:12:33 +0200

    Created a duplicate Cheep.cs in SimpleDB

--- src/Bison.CLI/Cheep.cs
+++ src/Bison.CLI/Cheep.cs
@@ -1,2 +1,4 @@
 
 public record Cheep(string Author, int ID, string Observation, long Timestamp, string Location);
+// OBS: Cheep findes nu 2 steder og er ren kodeduplikation. Skal nok slettes i Bison.CLI,
+// da både Bison.CLI og Bison.razor kan bruge cheep fra simpleDB.
---
+++ src/SimpleDB/Cheep.cs
@@ -0,0 +1,3 @@
+namespace SimpleDB;
+
+public record Cheep(string Author, int ID, string Observation, long Timestamp, string Location);
