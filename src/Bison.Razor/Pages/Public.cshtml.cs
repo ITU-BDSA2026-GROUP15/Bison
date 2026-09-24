6e5bd8ce70384e121fc74d6d2f9f9ef7d5508b32
Anne <akjn@itu.dk>
2026-09-24 09:57:28 +0200

    added os OnGet has the arugmets and can read the pagenumber from the URL og sent it.

--- src/Bison.Razor/Pages/Public.cshtml.cs
+++ src/Bison.Razor/Pages/Public.cshtml.cs
@@ -13,8 +13,9 @@ public class PublicModel : PageModel
         _service = service;
     }
 
-    public ActionResult OnGet()
-    {
+//Razor-siden til at læse sidetallet fra URL’en og sende det videre.
+    //[FromQuery] læser page fra URL’en,
+    public ActionResult OnGet([FromQuery] int page=1){
         Observations = _service.GetObservations();
         return Page();
     }
