ccdd3503d95fdc4aedb0570b2418ab53eca34b1b
Anne <akjn@itu.dk>
2026-09-24 09:53:23 +0200

    interface done, and the arguments are in the get methods

--- src/Bison.Razor/BisonService.cs
+++ src/Bison.Razor/BisonService.cs
@@ -1,38 +1,30 @@
 public record ObservationViewModel(string Author, string Message, string Timestamp);
 
-public interface IObservationService
-{
+public interface IObservationService{
     public List<ObservationViewModel> GetObservations(int page = 1);
     public List<ObservationViewModel> GetObservationsFromAuthor(string author, int page = 1);
-    // som standard er sidetallet 1.
-    //GetObservations(int page = 1);
-    //GetObservationsFromAuthor(string author, int page);
+    // som standard er sidetallet 1 og valgfrit.
 
 }
 
-public class ObservationService : IObservationService
-{
+public class ObservationService : IObservationService {
     // These would normally be loaded from a database for example
-    private static readonly List<ObservationViewModel> _obs = new()
-        {
+    private static readonly List<ObservationViewModel> _obs = new(){
             
             new ObservationViewModel("Peter", "I saw a heron", UnixTimeStampToDateTimeString(1690892208)),
             new ObservationViewModel("Paul", "There is a bison on Amager", UnixTimeStampToDateTimeString(1690895308)),
-        };
+    };
 
-    public List<ObservationViewModel> GetObservations()
-    {
+    public List<ObservationViewModel> GetObservations(int page =1){
         return _obs;
     }
 
-    public List<ObservationViewModel> GetObservationsFromAuthor(string author)
-    {
+    public List<ObservationViewModel> GetObservationsFromAuthor(string author, int page =1){
         // filter by the provided author name
         return _obs.Where(x => x.Author == author).ToList();
     }
 
-    private static string UnixTimeStampToDateTimeString(double unixTimeStamp)
-    {
+    private static string UnixTimeStampToDateTimeString(double unixTimeStamp) {
         // Unix timestamp is seconds past epoch
         DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
         dateTime = dateTime.AddSeconds(unixTimeStamp);
