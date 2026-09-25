05e32bbb49c1ae28f45625357772068fa632115c
Lærke Vestergaard Roest <lroe@itu.dk>
2026-09-25 10:38:43 +0200

    Added DBFacade.cs and the sceleton to add logic later

---
+++ src/Bison.Razor/DBFacade.cs
@@ -0,0 +1,37 @@
+using Microsoft.Data.Sqlite;
+
+public class DBFacade
+{
+    private readonly string _dbPath;
+
+    public DBFacade(string dbPath)
+    {
+        //her skal stien gemmes i feltet
+    }
+
+    public List<ObservationViewModel> GetObservations()
+    {
+        var result = new List<ObservationViewModel>();
+
+        using var connection = new SqliteConnection($"Data Source={_dbPath}");
+        connection.Open();
+
+        var command = connection.CreateCommand();
+        command.CommandText = @"TODO: din forespørgsel A";
+
+        using var reader = command.ExecuteReader();
+        while (reader.Read())
+        {
+            //her skal der læses de tre kolloner
+        }
+
+        return result;
+    }
+
+    public List<ObservationViewModel> GetObservationsFromAuthor(string author)
+    {
+        // forespørgsel B
+
+        return 
+    }
+}
