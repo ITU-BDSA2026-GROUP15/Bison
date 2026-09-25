c6e8df51433b9f356de1080e20471176b2fc0723
Lærke Vestergaard Roest <lroe@itu.dk>
2026-09-25 12:43:45 +0200

    Copied UnixTimeStampToDateTimeString method to DBFacade from BisonService.cs

--- src/Bison.Razor/DBFacade.cs
+++ src/Bison.Razor/DBFacade.cs
@@ -57,4 +57,11 @@ public class DBFacade
 
         return result;
     }
+    private static string UnixTimeStampToDateTimeString(double unixTimeStamp) // copied from BisonService.cs
+    {
+        // Unix timestamp is seconds past epoch
+        DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+        dateTime = dateTime.AddSeconds(unixTimeStamp);
+        return dateTime.ToString("MM/dd/yy H:mm:ss");
+    }
 }
