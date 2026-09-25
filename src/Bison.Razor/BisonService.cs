7e939301df9b350f64c6e4230898a288525b35ec
Lærke Vestergaard Roest <lroe@itu.dk>
2026-09-25 16:01:49 +0200

    Removed duplicate method from BisonService, as method is moved to DBFacade instead

--- src/Bison.Razor/BisonService.cs
+++ src/Bison.Razor/BisonService.cs
@@ -25,12 +25,4 @@ public class ObservationService : IObservationService
         return _db.GetObservationsFromAuthor(author);
     }
 
-    private static string UnixTimeStampToDateTimeString(double unixTimeStamp)
-    {
-        // Unix timestamp is seconds past epoch
-        DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
-        dateTime = dateTime.AddSeconds(unixTimeStamp);
-        return dateTime.ToString("MM/dd/yy H:mm:ss");
-    }
-
 }
